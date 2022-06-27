15ad409be7bda0492ce600addfb7747b069b256f
Arthur Carvalho <arthurhrc@gmail.com>
2022-06-27 14:32:45 -0300

    Removendo comentários e Atualizando o Estado

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment ({ content, onDeleteComment }) { 
-    function handleDeleteComment (){ /*O compartilhamento de função entre componentes ocorre aqui */
+    function handleDeleteComment (){ /*O compartilhamento de função entre componentes ocorre aqui, já que a função deletecomment está declarada no componente Post.jsx */
         console.log('deletar')
         onDeleteComment (content);
     }
@@ -20,9 +20,9 @@ export function Comment ({ content, onDeleteComment }) {
                             <strong>Arthur Carvalho</strong>
                             <time title="23 de junho às 11:02" dateTime="2022-06-23 11:02">Publicado há 1hr</time>
                         </div>
-                        <button onClick={handleDeleteComment} title="Deletar comentário">  /*Handle é representado pela ação do usuario*/
+                        <button onClick={handleDeleteComment} title="Deletar comentário">  
                             <Trash size={24} />
-                        </button>
+                        </button> 
                     </header>
 
                     <p>{content}</p>
